668c6ac53382b18c06ebfea98d996ead67ee7ca7
timgor <egor.vladone@gmail.com>
2018-12-31 15:46:06 -0500

    it draws circles

--- game.js
+++ game.js
@@ -3,7 +3,8 @@ var context
 
 
 function init(){
-	canvas = document.getElementById("canvas")
+	$("body").on("resize",resize)
+	canvas = $("#canvas").get()[0]
 	context = canvas.getContext("2d")
 	resize()
 }
@@ -15,16 +16,24 @@ function resize(){
 
 }
 
-function drawTile(rad, theta){
-	var rTotal = Math.min(canvas.width,canvas.height)/2 - 5
-	var r =  rad * rTotal/8
+function drawTile(rad, theta, color){
+	var rTotal = Math.min(canvas.width,canvas.height)/2 - 20
+	var r1 =  rad * rTotal/8
+	var r2 =  (rad + 1) * rTotal/8
 
 	context.beginPath()
-	context.arc(canvas.width/2,canvas.height/2,r,0,2*Math.PI)
+	context.arc(canvas.width/2,canvas.height/2,r1,theta,Math.PI/8 + theta)
+	context.arc(canvas.width/2,canvas.height/2,r2,Math.PI/8 + theta,theta,true)
+	context.lineTo(canvas.width/2 + r1*Math.cos(theta),canvas.height/2 +r1*Math.sin(theta))
+	context.closePath()
+	context.fillStyle = color;
+	context.fill()
 	context.stroke()
 }
 
 function drawBoard(){
-	drawTile(8)
-	drawTile(7)
+	drawTile(8,0,"rosybrown")
+
 }
+
+$(document).ready(init)
