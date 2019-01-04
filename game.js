16ef46e6b4eb6914bdd5651fee5a511dded750ab
timgor <egor.vladone@gmail.com>
2019-01-03 23:11:07 -0500

    added move checking for pawn, king, rook

--- game.js
+++ game.js
@@ -33,39 +33,31 @@ var currentPlayer = 'white'
 
 function init(){
 
+	var side = ["black","white"];
+	for(i in side){
+		console.log(i,side[i]);
+		pieces.push(new Piece(8,5+8*i,"rook",side[i]));
+		pieces.push(new Piece(8,2+8*i,"rook",side[i]));
+		pieces.push(new Piece(7,3+8*i,"bishop",side[i]));
+		pieces.push(new Piece(7,4+8*i,"bishop",side[i]));
+		pieces.push(new Piece(6,3+8*i,"knight",side[i]));
+		pieces.push(new Piece(6,4+8*i,"knight",side[i]));
+		pieces.push(new Piece(8,1+8*i,"pawn",side[i]));
+		pieces.push(new Piece(7,2+8*i,"pawn",side[i]));
+		pieces.push(new Piece(6,2+8*i,"pawn",side[i]));
+		pieces.push(new Piece(5,3+8*i,"pawn",side[i]));
+		pieces.push(new Piece(5,4+8*i,"pawn",side[i]));
+		pieces.push(new Piece(6,5+8*i,"pawn",side[i]));
+		pieces.push(new Piece(7,5+8*i,"pawn",side[i]));
+		pieces.push(new Piece(8,6+8*i,"pawn",side[i]));
+	}
 	pieces.push(new Piece(8,3,"king","black"));
 	pieces.push(new Piece(8,4,"queen","black"));
-	pieces.push(new Piece(8,5,"rook","black"));
-	pieces.push(new Piece(8,2,"rook","black"));
-	pieces.push(new Piece(7,3,"bishop","black"));
-	pieces.push(new Piece(7,4,"bishop","black"));
-	pieces.push(new Piece(6,3,"knight","black"));
-	pieces.push(new Piece(6,4,"knight","black"));
-	pieces.push(new Piece(8,1,"pawn","black"));
-	pieces.push(new Piece(7,2,"pawn","black"));
-	pieces.push(new Piece(6,2,"pawn","black"));
-	pieces.push(new Piece(5,3,"pawn","black"));
-	pieces.push(new Piece(5,4,"pawn","black"));
-	pieces.push(new Piece(6,5,"pawn","black"));
-	pieces.push(new Piece(7,5,"pawn","black"));
-	pieces.push(new Piece(8,6,"pawn","black"));
-	
 	pieces.push(new Piece(8,12,"king","white"));
 	pieces.push(new Piece(8,11,"queen","white"));
-	pieces.push(new Piece(8,5+8,"rook","white"));
-	pieces.push(new Piece(8,2+8,"rook","white"));
-	pieces.push(new Piece(7,3+8,"bishop","white"));
-	pieces.push(new Piece(7,4+8,"bishop","white"));
-	pieces.push(new Piece(6,3+8,"knight","white"));
-	pieces.push(new Piece(6,4+8,"knight","white"));
-	pieces.push(new Piece(8,1+8,"pawn","white"));
-	pieces.push(new Piece(7,2+8,"pawn","white"));
-	pieces.push(new Piece(6,2+8,"pawn","white"));
-	pieces.push(new Piece(5,3+8,"pawn","white"));
-	pieces.push(new Piece(5,4+8,"pawn","white"));
-	pieces.push(new Piece(6,5+8,"pawn","white"));
-	pieces.push(new Piece(7,5+8,"pawn","white"));
-	pieces.push(new Piece(8,6+8,"pawn","white"));
+
+	pieces.push(new Piece(4,4,"rook","white"));
+	
 	
 
 	canvas = $("#canvas").get()[0];
@@ -87,6 +79,11 @@ function init(){
 		// console.log('('+x+','+y+')');
 		// console.log(rad);
 		// console.log(angle);
+
+		for (var i = highlighted.length - 1; i >= 0; i--) {
+			highlighted.splice(i);
+		}
+
 		var rotateBoardThisTurn = false;
 		console.log(pieceAt(rad,angle));
 		if(selected == null && typeof pieceAt(rad,angle) !== 'undefined'){
@@ -94,11 +91,21 @@ function init(){
 			if(pieceAt(rad,angle).pColor == currentPlayer){
 				console.log("selected")
 				selected = pieceAt(rad,angle);
+				for (var j = 0; j <= 8; j++) {
+					for (var i = 0; i < 16; i++) {
+						if(isMoveLegal(j,i,selected)){
+							highlighted.push([j,i]);
+						}
+					}
+				}
 			}
+
+
 		} else if(isMoveLegal(rad,angle,selected) ){
 			var index = pieces.indexOf(pieceAt(rad, angle));
 			if (index !== -1) pieces.splice(index, 1);
 			selected.move(rad,angle);
+			selected.hasMoved = true;
 			selected = null;
 			move++;
 			redraw();
@@ -108,6 +115,8 @@ function init(){
 				currentPlayer = 'white';
 				rotateBoardThisTurn = true;
 			}
+		} else {
+			selected = null;
 		}
 
 		console.log(selected);
@@ -122,12 +131,26 @@ function init(){
 	});
 
 	window.onresize = redraw;
+
+	for(var i in blackPieceImgs){
+		blackPieceImgs[i].onload = redraw;
+	}
+
+	for(var i in whitePieceImgs){
+		whitePieceImgs[i].onload = redrawTwice;
+	}
+
 	redraw();
 
 }
 
+function redrawTwice(){
+	redraw();
+	redraw();
+}
+
 function redraw(h=0){
-	context.clearRect(0, 0, canvas.width, canvas.height);
+	
 	canvas.width = window.innerWidth - 10;
 	canvas.height = window.innerHeight -110;
 	rTotal = Math.min(canvas.width,canvas.height)/2 - 35;
@@ -201,7 +224,7 @@ function drawBoard(h){
 function rotateBoard(){
 	console.log("rotateBoard")
 	
-	var time = 200;
+	var time = 1000;
 	var interval = 10;
 	var step = interval/time;
 	var h = 0;
@@ -244,6 +267,7 @@ class Piece {
 		this.angle = angle;
 		this.pType = pType;
 		this.pColor = pColor;
+		this.hasMoved = false;
 	
 	}
 
@@ -251,6 +275,9 @@ class Piece {
 		this.rad = rad;
 		while(angle>15){
 			angle -= 16
+		}
+		if(rad == 0){
+			angle = 0;
 		}
 		this.angle = angle;
 	}
@@ -265,9 +292,6 @@ class Piece {
 		if(this.pColor == "white"){
 			img = whitePieceImgs[this.pType];
 		}
-
-		console.log(img.src);
-
 		
 		if(optRad == 0){
 			optAngle = 11.5
@@ -305,32 +329,65 @@ function pieceAt(rad,angle){
 function isMoveLegal(rad,angle,piece){
 	var moveTypes = {
 		"pawn" : function(rad,angle,piece){
-			console.log("checking for pawn")
+			// console.log("checking for pawn")
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
 					return false;
 				}
+
+				var da = Math.abs(angle-piece.angle);
+				da = Math.min(da, 16 - da);
+				if(da == 1 && rad == piece.rad-1){
+					return true;
+				}
 			}
-			return true;
+
+			if(typeof pieceAt(piece.rad-1, angle) !== 'undefined'){
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
+				if(pieceAt(piece.rad-1, angle).pColor == piece.pColor){
+					return false;
+				}
+			}
+
+			if(angle == piece.angle && rad == piece.rad-1){
+				return true;
+			}
+
+			if(!piece.hasMoved && angle == piece.angle && rad == piece.rad-2){
+				return true;
+			}
+
+			return false;
 		},
 		"king" : function(rad,angle,piece){
-			console.log("checking for pawn")
+			// console.log("checking for pawn")
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
 					return false;
 				}
+
+				if(Math.abs(angle - piece.angle) <= 1 && Math.abs(rad - piece.rad) <= 1){
+					return true
+				}
+
 			}
+
+			if(Math.abs(angle - piece.angle) <= 1 && Math.abs(rad - piece.rad) <= 1){
+				return true
+			}
+
 			return true;
 		},
 		"knight" : function(rad,angle,piece){
-			console.log("checking for pawn")
+			// console.log("checking for pawn")
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
 					return false;
 				}
@@ -338,10 +395,10 @@ function isMoveLegal(rad,angle,piece){
 			return true;
 		},
 		"queen" : function(rad,angle,piece){
-			console.log("checking for pawn")
+			// console.log("checking for pawn")
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
 					return false;
 				}
@@ -349,10 +406,10 @@ function isMoveLegal(rad,angle,piece){
 			return true;
 		},
 		"bishop" : function(rad,angle,piece){
-			console.log("checking for pawn")
+			// console.log("checking for pawn")
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
 					return false;
 				}
@@ -360,14 +417,86 @@ function isMoveLegal(rad,angle,piece){
 			return true;
 		},
 		"rook" : function(rad,angle,piece){
-			console.log("checking for pawn")
+
+			if(rad == 0){
+				return false;
+			}
+			// console.log("checking for pawn")
+			if(!(piece.rad == rad || piece.angle == angle)){
+				return false;
+			}
+
 			if(typeof pieceAt(rad, angle) !== 'undefined'){
-				console.log(pieceAt(rad, angle).pColor);
-				console.log(piece.pColor);
+				// console.log(pieceAt(rad, angle).pColor);
+				// console.log(piece.pColor);
 				if(pieceAt(rad, angle).pColor == piece.pColor){
-					return false;
+					return false;				
 				}
 			}
+
+			if(piece.rad == rad){
+				
+				var i = angle+1;
+				var canMoveCW = true;
+				var canMoveCCW = true;
+
+				while(i != piece.angle){
+					highlightTile(rad,i);	
+					if(typeof pieceAt(rad, i) !== 'undefined'){
+						canMoveCCW = false;
+					}
+					i++;
+					if(i>15){
+						i -= 16;
+
+					}
+
+				}
+
+				i = angle - 1;
+				if(i<0){
+					i += 16;
+				}
+
+				while(i != piece.angle){
+					highlightTile(rad,i);	
+					if(typeof pieceAt(rad, i) !== 'undefined'){
+						canMoveCW = false;
+					}
+					i--;
+					if(i<0){
+						i += 18;
+					}
+
+				}
+
+				if(!canMoveCW && !canMoveCCW){
+					return false;
+				}
+
+			}
+			if(piece.angle == angle && piece.rad < rad){
+				//out
+				console.log("out")
+				console.log(piece.rad,rad,rad>piece.rad)
+				for(var i = piece.rad+1; i < rad; i++){
+					if(typeof pieceAt(i, angle) !== 'undefined'){
+						return false;
+					}
+				}
+			}
+			if(piece.angle == angle && piece.rad > rad){
+				//in
+				console.log("in")
+				console.log(rad,piece.rad,rad<piece.rad)
+				for(var i = rad+1; i < piece.rad; i++){
+					console.log("============= checking for in")
+					if(typeof pieceAt(i, angle) !== 'undefined'){
+						return false;
+					}
+				}
+			}
+
 			return true;
 		}
 
@@ -379,6 +508,11 @@ function isMoveLegal(rad,angle,piece){
 	if(typeof legal == 'undefined'){
 		legal = false;
 	}
+	if(rad > 8 || rad < 0){
+		legal = false;
+	}
+
+	console.log("isMoveLegal:",legal);
 	return legal;
 }
 
